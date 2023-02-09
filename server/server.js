19a9c721a54d67a65c601e6f5e2c51705d64bd89
Pavel Vahovich <92326230+pasha01996@users.noreply.github.com>
2023-02-09 13:04:45 +0300

    init: add server

---
+++ server/server.js
@@ -0,0 +1,123 @@
+//----------------------------------express---------------------------------
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+const fs = require('fs');
+const PORT = 3000;
+const cors = require('cors');
+const app = express();
+const multer = require('multer');
+
+
+
+//-------------------------------------multer-------------------------------
+const storage= multer.diskStorage({
+    destination: (req, file, cb)=> {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}.JPG`)
+    }
+
+})
+const upload = multer({storage})
+
+
+//---------------------------------functions--------------------------------
+
+const writeFileAsync = async (path, data) => {
+    return new Promise((res, rej) => fs.writeFile(path, data, (err) => {
+        if (err) {return rej(err.message)}
+        res()
+    }))
+}
+
+const readFileAsync = async (path) => {
+    return new Promise((res, rej) => fs.readFile(path, {encoding: 'utf-8'}, (err, data) => {
+        if (err) {return rej(err.message)}
+        res(data)
+    }))
+}
+
+
+//--------------------------------server---------------------------------
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/uploads', express.static('uploads'))
+
+//--------------------------------server---------------------------------
+app.post('/users', async (req, res) => {
+
+    const minLength = 12
+
+    await readFileAsync(path.resolve(__dirname, 'users.txt'))
+        .then(data => { 
+
+        if (data.length > minLength) {
+        const lengthUsers = JSON.parse(data).length
+        const user = req.body
+        let allUsers = data
+
+        user.id = lengthUsers + 1
+        allUsers = allUsers.slice(1, -1)
+        allUsers = allUsers + ',' + JSON.stringify(user)
+        allUsers = '[' + allUsers + ']'
+
+        writeFileAsync(path.resolve(__dirname, 'users.txt'), allUsers)
+
+        } else {
+            const user = req.body
+            user.id = 1
+            writeFileAsync(path.resolve(__dirname, 'users.txt'), '['+ JSON.stringify(user) +']') 
+        }
+    })  
+
+})
+
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id
+    await readFileAsync(path.resolve(__dirname, 'users.txt'))
+    .then(data => {
+        const allUsers = JSON.parse(data)
+        const findUser = allUsers.find(user => user.id = id)
+        res.send(findUser)
+    })
+})
+
+app.get('/users', async (req, res) => {
+    await readFileAsync(path.resolve(__dirname, 'users.txt'))
+        .then(data => {res.send(data)})   
+})
+
+
+
+app.post('/overwriteuser', async (req, res) => {
+    await writeFileAsync(path.resolve(__dirname, 'users.txt'), JSON.stringify(req.body)) 
+})
+
+
+
+app.post('/uploads', upload.single('avatar'), async (req, res, next) => { 
+
+    fs.rename(`uploads/${req.file.fieldname}.JPG`, `uploads/${req.body.filename}.JPG`, (err) => { if(err) { throw err} })
+
+    await readFileAsync(path.resolve(__dirname, 'users.txt'))
+    .then(data => {
+        const allUsers = JSON.parse(data)
+        const findUser = allUsers.find(user => user.email === req.body.filename)
+        const findIndexUser = allUsers.findIndex(user => user.email === req.body.filename)
+
+        findUser.imgURL = `http://localhost:3030/uploads/${req.body.filename}.JPG`
+        allUsers.splice(findIndexUser, 1, findUser)
+        writeFileAsync(path.resolve(__dirname, 'users.txt'), JSON.stringify(allUsers))
+    })
+})  
+
+
+
+
+
+app.listen(PORT, () => console.log('Server start'))
+
+
+
